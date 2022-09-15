075f03230f46596e9c1dc6bff628dfb621fa6336
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-09-15 20:31:41 -0300

    feedpage in prog

--- labefoods/src/pages/feed-page/index.js
+++ labefoods/src/pages/feed-page/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"
+import { feedContainer } from "./style";
 
 export function Feedpage() {
 
     const navigate = useNavigate()
 
     return(
-        <>
-            <h1>Feed Page</h1>
-        </>
+        <feedContainer>
+            <h1>Ifuture</h1>
+            <input type="text"/>
+            <nav>
+                <a href="">Burger</a>
+                <a href="">Asiática</a>
+                <a href="">Massas</a>
+                <a href="">Saudáveis</a>
+            </nav>
+        </feedContainer>
     )
 }
---
+++ labefoods/src/pages/feed-page/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const feedContainer = styled.main`
+    display: flex;
+    border: 1px dotted gray;
+`
