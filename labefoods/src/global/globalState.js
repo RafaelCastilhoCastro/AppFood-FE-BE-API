2b514e5d70ec529d43ae6f0fad76c7427c0e20d9
Rafael <rafaelquintanilha@hotmail.com>
2022-09-16 21:16:26 -0300

    global changes

--- labefoods/src/global/globalState.js
+++ labefoods/src/global/globalState.js
@@ -1,17 +1,43 @@
 import React from "react";
 import { GlobalStateContext } from "./globalStateContext";
+import { useRequestData } from './../hooks/useRequestData';
+import { BASE_URL } from './../constants/constants';
 
 
 export function GlobalState(props) {
 
     // STATES
 
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [cpf, setCpf] = useState("")
+    const [pwd, setPwd] = useState("")
+    const [pwdConfirmation, setPwdConfirmation] = useState("")
+    const [restaurantsData, loadingRestaurants, errorRestaurants] = useRequestData(`${BASE_URL}restaurants`);
+    const [restaurantInfoData, loadingRestaurantInfo, errorRestaurantInfo] = useRequestData(`${BASE_URL}restaurants/${'id'}`);
+
     // EFFECTS
 
     // REQUESTS
 
+
+
     return (
-        <GlobalStateContext.Provider >
+        <GlobalStateContext.Provider
+            value={
+                {
+                    name,
+                    setName,
+                    email,
+                    setEmail,
+                    cpf,
+                    setCpf,
+                    pwd,
+                    setPwd,
+                    pwdConfirmation,
+                    setPwdConfirmation
+                }
+            }>
             {props.children}
         </GlobalStateContext.Provider>
     )
