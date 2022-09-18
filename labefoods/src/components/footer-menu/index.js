b0fbe90d7d0f4fd973052de7e131e7ed201b3df3
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-09-18 18:33:35 -0300

    Adds feedpage requisition

--- labefoods/src/components/footer-menu/index.js
+++ labefoods/src/components/footer-menu/index.js
@@ -4,9 +4,9 @@ import { FooterContainer } from "./style";
 export function FooterMenu(){
     return(
         <FooterContainer>
-            <i class="fa fa-home fa-2x" aria-hidden="true"></i>
-            <i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
-            <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+            <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+            <i className="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
+            <i className="fa fa-user fa-2x" aria-hidden="true"></i>
         </FooterContainer>
     )
 }
--- labefoods/src/style.js
+++ labefoods/src/style.js
@@ -2,6 +2,8 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     * {
+        --title-red: rgba(232,38,49,255);
+        --subtitle-gray: rgba(187,187,187,255);
         margin: 0;
         padding: 0;
         box-sizing: border-box;
