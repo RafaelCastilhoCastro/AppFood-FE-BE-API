f5b43ad86c0a4ca9240c6e7e85b2167dad8787a1
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-09-20 19:51:07 -0300

    removes useProtectedPage import from homepage

--- labefoods/src/pages/home/index.js
+++ labefoods/src/pages/home/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import gif from '../../img/loading-gif.gif'
 import { HomePageContainer } from "./style";
-import { useProtectedPage } from "../../hooks/useProtectedPage";
 import { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { goToFeedPage } from '../../routes/Coordinator';
