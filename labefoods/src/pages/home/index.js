772ed210ccaa8d21334b6768d4f5b588d5218a90
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-09-20 19:40:53 -0300

    Adds transition from homepage to feedpage

--- labefoods/src/pages/home/index.js
+++ labefoods/src/pages/home/index.js
@@ -2,13 +2,25 @@ import React from "react";
 import gif from '../../img/loading-gif.gif'
 import { HomePageContainer } from "./style";
 import { useProtectedPage } from "../../hooks/useProtectedPage";
+import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { goToFeedPage } from '../../routes/Coordinator';
 
 export function HomePage() {
     useProtectedPage()
 
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        setTimeout(() => {
+            goToFeedPage(navigate)
+        }, 3000)
+    }, [])
+    
+
     return (
         <HomePageContainer>
-            <img src={gif} alt='gif' />
+            <img src={gif} alt='gif'/>
         </HomePageContainer>
     )
 }
