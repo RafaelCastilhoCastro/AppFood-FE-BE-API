39163e41151870700604c369090f69e8bd5c2876
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-09-20 19:26:37 -0300

    links useProtectedPage to homepage

--- labefoods/src/pages/home/index.js
+++ labefoods/src/pages/home/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import gif from '../../img/loading-gif.gif'
 import { HomePageContainer } from "./style";
+import { useProtectedPage } from "../../hooks/useProtectedPage";
 
 export function HomePage() {
-
+    useProtectedPage()
 
     return (
         <HomePageContainer>
