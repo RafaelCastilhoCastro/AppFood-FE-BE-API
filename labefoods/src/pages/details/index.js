6e99fcff35d3050ec71a4fb512cefb2cbf427808
Rafael <rafaelquintanilha@hotmail.com>
2022-09-27 02:25:01 -0300

    addtothecart logic finished - missing style and remove

--- labefoods/src/pages/details/index.js
+++ labefoods/src/pages/details/index.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate, useParams } from "react-router-dom";
 import { BASE_URL } from "../../constants/constants";
 import { useRequestData } from '../../hooks/useRequestData';
 import * as All from './style';
 import { Header, LoadingDiv } from '../../components';
 import gif from '../../img/loading-gif.gif'
+import { GlobalStateContext } from './../../global/globalStateContext';
+import { useState } from 'react';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import { MenuItem, Select } from '@mui/material';
 
 
 
@@ -12,12 +17,44 @@ export function DetailsPage() {
 
     const navigate = useNavigate();
     const pathParams = useParams();
+    const { cartArray, setCartArray, totalValue, setTotalValue } = useContext(GlobalStateContext)
+    const [popQty, setPopQty] = useState(false)
+    const [itemQty, setItemQty] = useState(0)
+    const [selectedItem, setSelectedItem] = useState([])
 
     // REQUEST
 
     const [restaurantData, isLoading, error] = useRequestData(`${BASE_URL}restaurants/${pathParams.id}`)
 
     const detailsArray = [{ ...restaurantData }]
+
+    // FUNCTIONS
+
+    const addProduct = (qty) => {
+        if (qty > 0) {
+            const exists = cartArray.find((e) => e.id === selectedItem.id);
+        setTotalValue(totalValue + selectedItem.price * qty)
+        if (exists) {
+            setCartArray(
+                cartArray.map((e) =>
+                    e.id === selectedItem.id ? { ...exists, quantity: exists.quantity + qty } : e
+                )
+            )
+        } else {
+            setCartArray([...cartArray, { ...selectedItem, quantity: qty }])
+        }
+        }
+        toggleQty()
+    }
+
+    const toggleQty = (product) => {
+        setPopQty(!popQty)
+        setSelectedItem(product)
+    }
+
+    const handleItemQty = (e) => {
+        setItemQty(e.target.value)
+    }
 
     // RENDER RESTAURANT DETAIL
 
@@ -43,8 +80,31 @@ export function DetailsPage() {
                                 <All.ItemName>{product.name}</All.ItemName>
                                 <All.ItemDetail>{product.description}</All.ItemDetail>
                                 <All.PriceItem>R${product.price.toFixed(0)},00
-                                    <All.AddButton>Adicionar</All.AddButton></All.PriceItem>
+                                    <All.AddButton onClick={()=> toggleQty(product)}>Adicionar</All.AddButton></All.PriceItem>
                             </All.TextDiv>
+                            {popQty &&
+                                <All.SetQty>
+                                    <span>Selecione a quantidade desejada</span>
+                                    <FormControl focused={false} sx={{ m: 1, width: '90%' }}>
+                                        <Select
+                                            value={itemQty}
+                                            onChange={handleItemQty}
+                                            displayEmpty
+                                            inputProps={{ 'aria-label': 'Without label' }}
+
+                                        >
+                                            <MenuItem value={0}>0</MenuItem>
+                                            <MenuItem value={1}>1</MenuItem>
+                                            <MenuItem value={2}>2</MenuItem>
+                                            <MenuItem value={3}>3</MenuItem>
+                                            <MenuItem value={4}>4</MenuItem>
+                                            <MenuItem value={5}>5</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                    <button onClick={() => addProduct(itemQty)}>ADICIONAR AO CARRINHO</button>
+                                </All.SetQty>
+
+                            }
                         </All.ContainerProd>
                     )
                 })
@@ -54,11 +114,11 @@ export function DetailsPage() {
     })
 
     return (
-        <>
+        <All.DetailsContainer>
             <Header buttonExists={true} pageTitle={'Restaurante'} />
             {isLoading && <LoadingDiv><img src={gif} alt="gif" /></LoadingDiv>}
             {!isLoading && restaurantData && detailsList}
             {!isLoading && !restaurantData && error}
-        </>
+        </All.DetailsContainer>
     )
 }
