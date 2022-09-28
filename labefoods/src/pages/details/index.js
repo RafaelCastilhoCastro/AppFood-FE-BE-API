75c12d0328bfd3377b38229fd7cac039280b579e
Rafael Castilho de Castro <raphaeldavant@hotmail.com>
2022-09-27 21:15:17 -0300

    Fix: Fix styled components imports on details

--- labefoods/src/pages/details/index.js
+++ labefoods/src/pages/details/index.js
@@ -9,8 +9,7 @@ import { useState } from 'react'
 import FormControl from '@mui/material/FormControl'
 import { MenuItem, Select } from '@mui/material'
 import { useRef } from 'react'
-import { AddButton, CardTextDiv, DescriptionContainer, DetailsContainer, DetailsInfoDiv, ItemCount, ItemDescription, ItemName, NameCountDiv, PriceDiv, PriceSpan, ProductCard, ProductImg, ProductsTitle, RemoveButton, RestaurantDescription, RestaurantImg, RestaurtTitle, SetQty, Test1 } from './style'
-
+import * as All from './style'
 
 
 export function DetailsPage() {
@@ -76,17 +75,17 @@ export function DetailsPage() {
 
     const detailsList = restaurantData && detailsArray.map(details => {
         return (
-            <DetailsInfoDiv key={details.restaurant.id}>
-                <RestaurantImg src={details.restaurant.logoUrl} alt='logo' />
-                <RestaurtTitle>{details.restaurant.name}</RestaurtTitle>
-                <RestaurantDescription>{details.restaurant.category}</RestaurantDescription>
-                <DescriptionContainer>
-                    <RestaurantDescription>{details.restaurant.deliveryTime} min</RestaurantDescription>
-                    <RestaurantDescription>Frete R${details.restaurant.shipping.toFixed(2)}</RestaurantDescription>
-                </DescriptionContainer>
-                <RestaurantDescription>{details.restaurant.address}</RestaurantDescription>
+            <All.DetailsInfoDiv key={details.restaurant.id}>
+                <All.RestaurantImg src={details.restaurant.logoUrl} alt='logo' />
+                <All.RestaurtTitle>{details.restaurant.name}</All.RestaurtTitle>
+                <All.RestaurantDescription>{details.restaurant.category}</All.RestaurantDescription>
+                <All.DescriptionContainer>
+                    <All.RestaurantDescription>{details.restaurant.deliveryTime} min</All.RestaurantDescription>
+                    <All.RestaurantDescription>Frete R${details.restaurant.shipping.toFixed(2)}</All.RestaurantDescription>
+                </All.DescriptionContainer>
+                <All.RestaurantDescription>{details.restaurant.address}</All.RestaurantDescription>
 
-                <ProductsTitle>Produtos</ProductsTitle>
+                <All.ProductsTitle>Produtos</All.ProductsTitle>
                 {details.restaurant.products.map(product => {
                     const exists = cartArray.find((e) => e.id === product.id)
                     if (!exists) {
@@ -95,22 +94,22 @@ export function DetailsPage() {
                         toggle.current = true
                     }
                     return (
-                        <ProductCard key={product.id}>
-                            <ProductImg src={product.photoUrl} />
-                            <CardTextDiv>
-                                <NameCountDiv>
-                                    <ItemName>{product.name}</ItemName>
-                                    {toggle.current && <ItemCount>{exists.quantity}</ItemCount>}
-                                </NameCountDiv>
-                                <ItemDescription>{product.description}</ItemDescription>
-                                <PriceDiv>
-                                    <PriceSpan>R${product.price.toFixed(2)}</PriceSpan>
-                                    {!toggle.current ? <AddButton onClick={() => toggleQty(product)}>adicionar</AddButton> : <RemoveButton onClick={() => { deleteProduct(product) }}>remover</RemoveButton>}
-                                </PriceDiv>
-                            </CardTextDiv>
+                        <All.ProductCard key={product.id}>
+                            <All.ProductImg src={product.photoUrl} />
+                            <All.CardTextDiv>
+                                <All.NameCountDiv>
+                                    <All.ItemName>{product.name}</All.ItemName>
+                                    {toggle.current && <All.ItemCount>{exists.quantity}</All.ItemCount>}
+                                </All.NameCountDiv>
+                                <All.ItemDescription>{product.description}</All.ItemDescription>
+                                <All.PriceDiv>
+                                    <All.PriceSpan>R${product.price.toFixed(2)}</All.PriceSpan>
+                                    {!toggle.current ? <All.AddButton onClick={() => toggleQty(product)}>adicionar</All.AddButton> : <All.RemoveButton onClick={() => { deleteProduct(product) }}>remover</All.RemoveButton>}
+                                </All.PriceDiv>
+                            </All.CardTextDiv>
                             {popQty &&
                                 <>
-                                    <SetQty>
+                                    <All.SetQty>
                                         <span>Selecione a quantidade desejada</span>
                                         <FormControl focused={false} sx={{ m: 1, width: '90%' }}>
                                             <Select
@@ -133,23 +132,23 @@ export function DetailsPage() {
                                             </Select>
                                         </FormControl>
                                         <button onClick={() => addProduct(itemQty)}>ADICIONAR AO CARRINHO</button>
-                                    </SetQty>
+                                    </All.SetQty>
                                 </>
                             }
-                        </ProductCard>
+                        </All.ProductCard>
                     )
                 })
                 }
-            </ DetailsInfoDiv>
+            </ All.DetailsInfoDiv>
         )
     })
 
     return (
-            <DetailsContainer toggleGrayBackground={toggleGrayBackground}>
+            <All.DetailsContainer toggleGrayBackground={toggleGrayBackground}>
                 <Header buttonExists={true} pageTitle={'Restaurante'} />
                 {isLoading && <LoadingDiv><img src={gif} alt="gif" /></LoadingDiv>}
                 {!isLoading && restaurantData && detailsList}
                 {!isLoading && !restaurantData && error}
-            </DetailsContainer>
+            </All.DetailsContainer>
     )
 }
